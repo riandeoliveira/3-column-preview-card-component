2fed5cfbc2fe9b528b4c6c02b2b066202f732233
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-04-14 11:20:48 -0300

    💎 style: Update ```media.ts```

--- src/assets/media.ts
+++ src/assets/media.ts
@@ -1,6 +1,6 @@
+import luxury from "./images/luxury.svg";
 import sedans from "./images/sedans.svg";
 import suvs from "./images/suvs.svg";
-import luxury from "./images/luxury.svg";
 
 const images = [sedans, suvs, luxury];
 
